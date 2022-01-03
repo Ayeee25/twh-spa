e22ccbd0cb9d0eb35de4c469196fc0b19e243097
EnriqueRojas23 <enrique.rojas@riabc.net>
2022-01-03 16:28:51 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('kama-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -20,7 +20,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['pogress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
